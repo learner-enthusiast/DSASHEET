051952f65301403d5d868b4ade56f03c08350d57
learner-enthusiast <loveumearnab.2812000@gmail.com>
2026-03-12 19:21:13 +0530

    feat: node insertion

---
+++ BST/index.js
@@ -0,0 +1,37 @@
+class BSTNode {
+  constructor(key) {
+    this.key = key;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+  insert(key) {
+    const newNode = BSTNode(key);
+
+    if (this.root === null) {
+      this.root = newNode;
+    } else {
+    }
+  }
+
+  insertNode(rootNode, newNode) {
+    if (newNode.key < rootNode.key) {
+      if (rootNode.left === null) {
+        rootNode.left = newNode;
+      } else {
+        this.insertNode(rootNode.left, newNode);
+      }
+    } else {
+      if (rootNode.right === null) {
+        rootNode.right = newNode;
+      } else {
+        this.insertNode(rootNode.right, newNode);
+      }
+    }
+  }
+}
