7ef1c70fa4d194d450abd3fa75c17cfacea39e8a
learner-enthusiast <loveumearnab.2812000@gmail.com>
2026-03-12 19:59:38 +0530

    fix: deletion bst

--- BST/index.js
+++ BST/index.js
@@ -35,4 +35,42 @@ class BinarySearchTree {
       }
     }
   }
+
+  delete(key) {
+    this.root = this.deleteNode(this.root, key);
+  }
+  deleteNode(rootNode, key) {
+    if (rootNode === null) {
+      return null;
+    }
+    if (key < rootNode.key) {
+      rootNode.left = this.deleteNode(rootNode.left, key); //HAs to modify the left side of node
+    } else if (key > rootNode.key) {
+      rootNode.right = this.deleteNode(rootNode.right, key); //Has to modify the right side of node
+    } else {
+      //Now we found the key
+
+      if (rootNode.left === null && rootNode.right === null) {
+        //Leaf node case
+        return null;
+      } else if (
+        (rootNode.right && !rootNode.left) ||
+        (!rootNode.right && rootNode.left)
+      ) {
+        return rootNode.right ? rootNode.right : rootNode.left;
+      } else {
+        //2child present
+        let tempNode = this.findMinNode(rootNode.right); //In order successor
+        rootNode.key = tempNode.key;
+        rootNode.right = this.deleteNode(rootNode.right, tempNode.key);
+      }
+    }
+    return rootNode;
+  }
+  findMinNode(node) {
+    while (node.left !== null) {
+      node = node.left;
+    }
+    return node;
+  }
 }
